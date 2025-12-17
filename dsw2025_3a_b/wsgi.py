787cb6b44e74d8ee633e4088dec4e4b8f6f1d073
Leonardo3202 <leonardodias0603@gmail.com>
2025-12-17 14:50:59 +0000

    Configuração para deploy

---
+++ dsw2025_3a_b/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dsw2025_3a_b project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dsw2025_3a_b.settings")
+
+application = get_wsgi_application()
